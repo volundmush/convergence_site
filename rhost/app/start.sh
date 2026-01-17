70e59bc6b3e80fff71ba47de4764be89675412b8
Neil Stevens <neil@hakubi.us>
2026-01-16 19:50:19 -0500

    We're a go

--- rhost/app/start.sh
+++ rhost/app/start.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
-
-cd /app
-git clone https://github.com/RhostMUSH/trunk.git rhost
+mkdir /rhost
+cd /rhost
+git clone https://github.com/RhostMUSH/trunk.git .
+cd Server
+cp /app/asksource.save_default bin/
+cp -r /app/data/*.conf game/
+cp -r /app/data/txt/* game/txt/
+make default
+make links
+cd game
+echo "Y\nY\n/app/data/data/netrhost.db.flat\n" | ./Startmush
+tail -f netrhost.log
