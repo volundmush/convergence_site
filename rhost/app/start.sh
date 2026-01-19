0f60b1ead25de400f7e1511da0dca7b5f01aadbb
Neil Stevens <neil@hakubi.us>
2026-01-18 22:16:28 -0500

    Build rhost a bit faster in most circumstances

--- rhost/app/start.sh
+++ rhost/app/start.sh
@@ -6,7 +6,7 @@ cd Server
 cp /app/asksource.save_default bin/
 cp -r /app/data/*.conf game/
 cp -r /app/data/txt/* game/txt/
-make default
+make default -j 4
 make links
 cd game
 echo "Y\nY\n/app/data/data/netrhost.db.flat\n" | ./Startmush
