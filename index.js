ce3cbe13eb20aa43e3acd3f99709fb4ea68260e1
Neil Stevens <neil@hakubi.us>
2026-02-17 00:19:56 -0500

    Fix rhostCheckLogin references

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ async function rhostCheckLogin(accountName, password, characterName = undefined)
 	const luaScript = `
 ret = {}
 accountRef = rhost.strfunc("eval", "namegrab(searchngobjid(TOTEMS=A), ${escapeInput(accountName)})")
-checkPass = rhost.strfunc("eval", "attrpass(", accountRef .. "/_PASSWORD, ${escapeInput(password)}, chk") == "1"
+checkPass = rhost.strfunc("eval", "attrpass(" .. accountRef .. "/_PASSWORD, ${escapeInput(password)}, chk") == "1"
 characterRef = rhost.strfunc("pmatch", "${escapeInput(characterName)}")
 hasCharacter = rhost.strfunc("eval", "[streq(get(" .. characterRef .. "/_ACCOUNT), " .. accountRef .. ")]") == "1"
 if hasCharacter and checkPass then
