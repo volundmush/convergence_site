ab26125b17a5c90ad21146bf99f9333d4e78bbbb
Neil Stevens <neil@hakubi.us>
2026-02-18 21:56:15 -0500

    Trigger happy

--- index.js
+++ index.js
@@ -393,7 +393,7 @@ return json.encode(char)
 
 			const triggerInfo = async (type, val) => {
 				const script = `
-str = rhost.strfunc("trigger", "#info/do_info,,,,${checkLogin.characterRef},/,${type},${escapeInput(val)}")
+str = rhost.strfunc("trigger", "#info/do_info%b%b%b%b${checkLogin.characterRef}%b/%b${type}%b{${escapeInput(val)}}",%b)
 return '"' .. str .. '"'
 `
 				return await rhostLua(script)
