aace554d1556614cc031fc4de46f60690a712183
Neil Stevens <neil@hakubi.us>
2026-03-05 10:46:29 -0500

    Move build time?

--- keystone/entrypoint.sh
+++ keystone/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "📦 Checking dependencies..."
-if [ ! -d "node_modules" ]; then
-  echo "📥 node_modules not found, installing dependencies..."
-  pnpm install
-else
-  echo "✓ Dependencies found"
-fi
-
-echo "🔨 Building Keystone..."
-pnpm build
-
 echo "🚀 Starting Keystone..."
-pnpm start
+exec pnpm start
