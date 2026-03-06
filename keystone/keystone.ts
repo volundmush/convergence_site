b13179f0516e448e9a3998816e0a802e7c921b5e
Neil Stevens <neil@hakubi.us>
2026-03-05 23:39:40 -0500

    Testing

--- keystone/keystone.ts
+++ keystone/keystone.ts
@@ -31,7 +31,6 @@ export default config({
 	server: {
 		cors: { origin: true, credentials: true },
 		extendExpressApp: (app, commonContext) => {
-			console.log('[Keystone] extendExpressApp called');
 			app.set('trust proxy', true);
 			const cookieParser = require('cookie-parser');
 			app.use(cookieParser());
