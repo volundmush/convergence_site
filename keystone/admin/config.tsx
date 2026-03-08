57995bb1030faf7b2982c2e1ca387fe946631d5a
Neil Stevens <neil@hakubi.us>
2026-03-08 00:34:42 -0500

    Branding

---
+++ keystone/admin/config.tsx
@@ -0,0 +1,9 @@
+import { Heading } from '@keystar/ui/typography';
+
+function CustomLogo() {
+	return <Heading type="h3">Convergence MUSH</Heading>;
+}
+
+export const components = {
+	Logo: CustomLogo,
+};
