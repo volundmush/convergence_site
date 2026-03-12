f4e47620f0903d1e7e02afe5f98fa95458def283
Neil Stevens <neil@hakubi.us>
2026-03-11 21:26:00 -0400

    Logging

--- keystone/keystone.ts
+++ keystone/keystone.ts
@@ -73,6 +73,7 @@ export default config({
 		basePath: '/admin',
 		isAccessAllowed: async (context) => {
 			const req = context.req as any;
+			console.log("[isAccessAllowed] auth", req?.cookies?.auth)
 			
 			// Check JWT cookie
 			if (req?.cookies?.auth) {
@@ -80,8 +81,10 @@ export default config({
 					const JWT_SECRET = process.env.JWT_SECRET || 'change-this-in-production';
 					const jwtSecret = new TextEncoder().encode(JWT_SECRET);
 					await jose.jwtVerify(req.cookies.auth, jwtSecret);
+					console.log("[isAccessAllowed] token verified")
 					return true;
 				} catch (e) {
+					console.log("[isAccessAllowed] token did not verify")
 					// Invalid JWT, fall through
 				}
 			}
